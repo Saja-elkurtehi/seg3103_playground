86c93256f28a0ba490529af668b3a4719444bc65
Saja Elkurtehi <114425874+Saja-elkurtehi@users.noreply.github.com>
2024-06-06 18:14:56 -0400

    Implement square function to pass the test

--- lab04/calculator/lib/Calculator.ex
+++ lab04/calculator/lib/Calculator.ex
@@ -24,7 +24,10 @@ defmodule Calculator do
   def divide(a, b) when b != 0, do: a / b
   def divide(_, 0), do: {:error, "Division by zero"}
 
- 
+  @doc """
+  Squares a number.
+  """
+  def square(a), do: a * a
 
   @doc """
   Computes the square root of a number.
